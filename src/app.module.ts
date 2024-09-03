d79045aed8587c0ea479b0e8225ed5e9ba6f0bbc
pedro <pfaria.sjp@ecori.com.br>
2024-09-03 16:12:05 -0300

    criando validação de variaveis de ambiente e criando validação de campos com o zod

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma-service'
+import { CreateAccountController } from './controllers/create-account.controller'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env'
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: env => envSchema.parse(env),
+      isGlobal: true
+    })
+  ],
+  controllers: [
+    CreateAccountController
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
