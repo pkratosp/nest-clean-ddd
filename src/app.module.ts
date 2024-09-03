cd154f7b19106cd922e178aae4e200a6aba02c5b
pedro <pfaria.sjp@ecori.com.br>
2024-09-03 16:25:25 -0300

    iniciando configuração de token

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { PrismaService } from './prisma/prisma-service'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validate: env => envSchema.parse(env),
       isGlobal: true
-    })
+    }),
+    AuthModule
   ],
   controllers: [
     CreateAccountController
