b247660a9af6f20c9aa1f065689ac55f611d14ed
pedro <pfaria.sjp@ecori.com.br>
2024-09-12 08:01:33 -0300

    criando testes de questões

--- src/controllers/fetch-recent-question.controller.ts
+++ src/controllers/fetch-recent-question.controller.ts
@@ -24,7 +24,7 @@ export class FetchRecentQuestionController {
 
     @Get()
     async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
-        const perPage = 1
+        const perPage = 20
 
         const questions = await this.prismaService.questions.findMany({
             take: perPage,
